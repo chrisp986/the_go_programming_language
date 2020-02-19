150adb8ff324b473ec33e37d51515290b2b131a4
christian <cpeters986@gmail.com>
2020-02-19 07:00:08 +0100

    Working on exercise 2.2.

--- chapter2/ex2_2.go
+++ chapter2/ex2_2.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 )
 
 func main() {
-	arg := os.Args[1:]
-	if len(arg) > 0 {
-		arg := strings.Join(arg, " ")
-		convert(arg)
-	} else {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter text: ")
-		text, _ := reader.ReadString('\n')
-		convert(text)
+	var value float64
+	var rate float64
+	arg1 := os.Args[1]
+	arg2 := os.Args[2]
+	unit := os.Args[3]
+	value, _ = strconv.ParseFloat(arg1, 64)
+	rate, _ = strconv.ParseFloat(arg2, 64)
+	if value > 0 {
+		//arg := strings.Join(arg, " ")
+		convert(value, rate, unit)
 	}
+	//else {
+	//	reader := bufio.NewReader(os.Stdin)
+	//	fmt.Print("Enter text: ")
+	//	text, _ := reader.ReadString('\n')
+	//	convert(text)
+	//}
 }
 
 // TODO convert input and output it
-func convert(command string) {
-	fmt.Println(command)
+func convert(value float64, rate float64, unit string) {
+	fmt.Printf("Value: %.2f , Conversion rate: %.2f , Unit: %s", value, rate, unit)
 }
