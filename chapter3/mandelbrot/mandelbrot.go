56bb120e0ad506ddd2d43eaf59d1297db5954e10
christian <cpeters986@gmail.com>
2020-02-25 05:12:19 +0100

    Finished mandelbrot.go

--- chapter3/mandelbrot/mandelbrot.go
+++ chapter3/mandelbrot/mandelbrot.go
@@ -15,9 +15,9 @@ func main() {
 	)
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for py := 0; py < height; py++ {
+	for py := 0; py < height*; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
-		for px := 0; px < width; px++ {
+		for px := 0; px < width*; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 
@@ -36,8 +36,19 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			//return color.Gray{255 - contrast*n}
+			return color.RGBA{
+				R: 50+contrast,
+				G: n,
+				B: 255-contrast*n,
+				A: 255-n,
+			}
 		}
 	}
-	return color.Black
+	return color.RGBA{
+		R: 255-contrast,
+		G: 0,
+		B: 255-contrast*2,
+		A: 255,
+	}
 }
