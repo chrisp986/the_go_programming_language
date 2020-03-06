295dfd9834183d7f05541e7124355896e3347aef
christian <cpeters986@gmail.com>
2020-03-06 06:16:16 +0100

    Started printints.go and comma.go.

---
+++ chapter3/printints/printints.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func intToString(values []int) string {
+
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteString(", ")
+			n, _ := fmt.Fprintf(&buf, "%d", v)
+			fmt.Printf("This byte is so long %v\n", n)
+		}
+	}
+	buf.WriteByte(']')
+	return buf.String()
+}
+
+func main() {
+	fmt.Println(intToString([]int{1, 2, 3}), " this ")
+}
